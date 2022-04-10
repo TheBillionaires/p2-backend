a52a7efd6dd66114478cf7027784dd5e26bf6b32
TheBillionaires <102680541+TheBillionaires@users.noreply.github.com>
2022-04-09 22:15:43 -0700

    Add files via upload

---
+++ SpringEmailClient/src/test/java/com/nativcode/SpringEmailClient/SpringEmailClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nativcode.SpringEmailClient;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
